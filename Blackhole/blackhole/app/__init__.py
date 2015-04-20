cb8606a2a254fbba53271a87dbbc816e4fe31ce9
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2015-04-20 07:46:34 -0300

    Fix in app log definition

--- Blackhole/blackhole/app/__init__.py
+++ Blackhole/blackhole/app/__init__.py
@@ -6,11 +6,14 @@ from blackhole import settings
 
 logger = logging.getLogger("blackhole")
 logger.setLevel(settings.log_level)
-if settings.DEBUG:
-    if os.access("/tmp/blackhole.log", os.W_OK):
-        log_handler = logging.FileHandler("/tmp/blackhole.log")
+if settings.log_level == logging.DEBUG:
+    if os.path.isfile("/tmp/blackhole.log"):
+        if os.access("/tmp/blackhole.log", os.W_OK):
+            log_handler = logging.FileHandler("/tmp/blackhole.log")
+        else:
+            log_handler = SysLogHandler(address='/dev/log')
     else:
-        log_handler = SysLogHandler(address='/dev/log')
+        log_handler = logging.FileHandler("/tmp/blackhole.log")
 else:
     log_handler = SysLogHandler(address='/dev/log')
 log_handler.setFormatter(logging.Formatter('%(asctime)s - BlackHole - %(levelname)s: %(message)s'))
