0f6a28c71353df990f0a9407ac396b1740a6fbbc
laurent <laurent@bewie.org>
2015-06-19 13:17:41 +0200

    Add user password management interface

--- Blackhole/blackhole/urls.py
+++ Blackhole/blackhole/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +10,15 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'web/login.html'}),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/web/'}),
+
+    url(r'^password/change/$',
+        'django.contrib.auth.views.password_change',
+        {'template_name': 'web/password_change.html'},
+        name='password_change'),
+    url(r'^password/change/done/$',
+		RedirectView.as_view(url='/web/'),
+        name='password_change_done'),
+ 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^web/', include('web.urls')),
 )
